e5fdea167ba6faa19053b6714c7a7105b5c6005f
it21156960 <chamith227@gmail.com>
2023-06-25 00:05:16 +0530

    Initial commit

---
+++ src/test/java/com/example/SimpleEmployee/SimpleEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SimpleEmployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
