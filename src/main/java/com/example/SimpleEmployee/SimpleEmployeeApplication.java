feb860629f6d5c7f9487860a9b42815e211c3e30
it21156960 <chamith227@gmail.com>
2023-06-25 00:05:16 +0530

    Initial commit

---
+++ src/main/java/com/example/SimpleEmployee/SimpleEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.SimpleEmployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleEmployeeApplication.class, args);
+	}
+
+}
